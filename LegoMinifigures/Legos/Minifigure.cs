b25bdb1c0a046de2a5ec941ece9b8b7d77baf53b
albertchitta <albert.w.chittaphong@gmail.com>
2022-01-12 21:54:54 -0600

    Add project files.

---
+++ LegoMinifigures/Legos/Minifigure.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegoMinifigures.Legos
+{
+    class Minifigure
+    {
+        public string HairHat { get; set; }
+        public string Head { get; set; }
+        public string Torso { get; set; }
+        public string Legs { get; set; }
+        public string Accessories { get; set; }
+
+        public void Jump()
+        {
+            Console.WriteLine("Jump!");
+        }
+
+        public void DoubleJump()
+        {
+            Console.WriteLine("Double Jump!");
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine("Attack!");
+        }
+
+        public void SpecialAttack()
+        {
+            Console.WriteLine("Special Attack!");
+        }
+    }
+}
---
+++ LegoMinifigures/Program.cs
@@ -0,0 +1,32 @@
+﻿using LegoMinifigures.Legos;
+using System;
+
+namespace LegoMinifigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Minifigure cowboy = new Minifigure
+            {
+                HairHat = "Cowboy Hat",
+                Head = "Big",
+                Torso = "Brown Vest",
+                Legs = "Cowboy Pants",
+                Accessories = "Lasso",
+            };
+
+            Minifigure worker = new Minifigure
+            {
+                HairHat = "Curly Hair",
+                Head = "Glasses",
+                Torso = "Jacket",
+                Legs = "Dress Pants",
+                Accessories = "Briefcase",
+            };
+
+            cowboy.Attack();
+            worker.DoubleJump();
+        }
+    }
+}
